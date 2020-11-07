a20b63f418061bee44c79a49dd1cdf15ba16ddc8
Frank Wunderlich <frank-w@public-files.de>
2020-11-07 14:59:50 +0100

    build.sh: add simple build-script + conf

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+LANG=C
+export CROSS_COMPILE=/usr/bin/aarch64-linux-gnu-
+
+#DEVICE=emmc/sdmmc
+if [[ -e build.conf ]];then
+	. build.conf
+fi
+
+case $1 in
+	"upload")
+		scp ./build/mt7622/release/{bl2.img,fip.bin} bpi-r64.gpt $uploaduser@$uploadserver:$uploaddir
+	;;
+	"")
+		echo "device: $DEVICE"
+		if [[ -e u-boot.bin ]];then
+			set -x
+			make distclean
+			make PLAT=mt7622 BL33=u-boot.bin BOOT_DEVICE=$DEVICE all fip
+			set +x
+		else
+			echo "u-boot.bin missing!"
+		fi
+	;;
+esac
